ccd83507a6add18b6c98fede92134dc136c0c6de
Josh Cheek <josh.cheek@gmail.com>
2013-03-10 08:12:35 -0500

    Can print the pids

--- src/ips.hs
+++ src/ips.hs
@@ -1,2 +1,8 @@
+-- $ ps -eo pid
+import System.Process
+import Data.Text
+
 main = do
-  putStrLn "Hello World!"
+  let strip string = Data.Text.unpack $ Data.Text.strip $ Data.Text.pack string
+  string <- System.Process.readProcess "ps" ["-eo", "pid"] ""
+  print $ Prelude.map strip (Prelude.lines string)
