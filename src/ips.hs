2f5dbd98c53055565a3735f669e7dc8b53494894
Josh Cheek <josh.cheek@gmail.com>
2013-03-12 21:45:16 -0500

    Define the Process structure I want to use

--- src/ips.hs
+++ src/ips.hs
@@ -1,8 +1,12 @@
--- $ ps -eo pid
 import Prelude
 import System.Process
 import Data.Text (pack, unpack, strip, breakOn)
 -- import qualified Data.Text as T
+
+data ProcessPid     = ProcessPid     String
+data ProcessUser    = ProcessUser    String
+data ProcessCommand = ProcessCommand String
+data Process        = Process ProcessPid ProcessUser ProcessCommand
 
 main = do
   pids <- getProcesses
@@ -14,6 +18,9 @@ getProcesses = do
   let processes = map toProcess (withoutHeader $ lines rawProcesses)
   return processes
 
+-- apparently you can get pretty far with Data.Text.words, but since there doesn't seem to be
+-- a way to get ps to delimit things for a program to parse them, I'm going to keep using this for now
+-- even though it's gross, since it knows where the command is
 toProcess rawProcess =
   map unpack (
     (\line -> (\(pid, rest) -> (\(user, unstrippedCommand) -> [pid, user, (strip unstrippedCommand)])
