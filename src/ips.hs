e771705472804640364e5c1979d79c5b0810773c
Josh Cheek <josh.cheek@gmail.com>
2013-03-12 21:56:01 -0500

    Simplify invocations somewhat

--- src/ips.hs
+++ src/ips.hs
@@ -1,5 +1,5 @@
 import Prelude
-import System.Process
+import System.Process (readProcess)
 import Data.Text (pack, unpack, strip, breakOn)
 -- import qualified Data.Text as T
 
@@ -8,25 +8,24 @@ data ProcessUser    = ProcessUser    String deriving Show
 data ProcessCommand = ProcessCommand String deriving Show
 data Process        = Process ProcessPid ProcessUser ProcessCommand deriving Show
 
+
 main = do
   pids <- getProcesses
   print pids
 
 getProcesses = do
-  let withoutHeader = drop 1
-  rawProcesses <- System.Process.readProcess "ps" ["-eo", "pid user command"] ""
-  let processes = map toProcess (withoutHeader $ lines rawProcesses)
-  return processes
+  rawProcesses <- readProcess "ps" ["-eo", "pid user command"] ""
+  return $ map toProcess (withoutHeader $ lines rawProcesses)
+
+space         = pack " "
+withoutHeader = drop 1
 
 -- apparently you can get pretty far with Data.Text.words, but since there doesn't seem to be
 -- a way to get ps to delimit things for a program to parse them, I'm going to keep using this for now
 -- even though it's gross, since it knows where the command is
 toProcess rawProcess =
-  (\line -> (\(pid, rest) -> (\(user, unstrippedCommand) -> Process (ProcessPid     $ unpack pid)
-                                                                    (ProcessUser    $ unpack user)
-                                                                    (ProcessCommand $ unpack $ strip unstrippedCommand))
+  (\(pid, rest) -> (\(user, unstrippedCommand) -> Process (ProcessPid     $ unpack pid)
+                                                          (ProcessUser    $ unpack user)
+                                                          (ProcessCommand $ unpack $ strip unstrippedCommand))
                                (breakOn space (strip rest)))
-              (breakOn space line))
-    (strip $ pack rawProcess)
-
-space = pack " "
+    (breakOn space $ strip $ pack rawProcess)
