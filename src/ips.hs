457eeba4c3ec32ca51b7ac61bba32aaeb3d4e11f
Josh Cheek <josh.cheek@gmail.com>
2013-03-12 20:07:03 -0500

    Refactorings

--- src/ips.hs
+++ src/ips.hs
@@ -5,13 +5,13 @@ import Data.Text (pack, unpack, strip, breakOn)
 -- import qualified Data.Text as T
 
 main = do
-  -- print pids
   pids <- getProcesses
   print pids
 
 getProcesses = do
-  let space = pack " "
-  let toProcess rawProcess = map unpack (
+  let space                = pack " "
+      withoutHeader        = drop 1
+      toProcess rawProcess = map unpack (
         (\line -> (\(pid, rest) -> (\(user, unstrippedCommand) -> [pid, user, (strip unstrippedCommand)])
                                      (breakOn space (strip rest)))
                     (breakOn space line))
@@ -19,5 +19,5 @@ getProcesses = do
             (pack rawProcess)))
 
   rawProcesses <- System.Process.readProcess "ps" ["-eo", "pid user command"] ""
-  let pids = map toProcess (drop 1 $ lines rawProcesses)
+  let pids = map toProcess (withoutHeader $ lines rawProcesses)
   return pids
